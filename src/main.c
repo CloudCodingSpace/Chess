9317b212277b5e35bb6a04e870f8cd2e3d28979f
CloudCodingSpace <subhamaygamer2127@outlook.com>
2026-01-21 18:39:10 +0530

    Opened a window

--- src/main.c
+++ src/main.c
@@ -1,7 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define null 0
+
+#define INFO(msg, ...) fprintf(stdout, "INFO: "msg, ##__VA_ARGS__)
+#define ERROR(msg, ...) fprintf(stderr, "ERROR: "msg, ##__VA_ARGS__)
+#define ASSERT(expr, msg, ...) do { if(!(expr)) { fprintf(stderr, "ASSERT FAILED! "msg, \
+                       ##__VA_ARGS__); exit(1); } } while(0)
+
+typedef struct {
+    GLFWwindow* window;
+    int width, height;
+} Ctx;
 
 int main(void) {
-    printf("Hello World\n");
+    Ctx ctx = {
+        .width = 1260,
+        .height = 720
+    };
+
+    // Init 
+    {
+        // Window
+        {
+            ASSERT(glfwInit(), "Can't init glfw!\n");
+
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+            glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+            ctx.window = glfwCreateWindow(ctx.width, ctx.height, "Chess", null, null);
+            ASSERT(ctx.window, "Can't create window!\n");
+
+            glfwMakeContextCurrent(ctx.window);
+
+            ASSERT(gladLoadGLLoader((GLADloadproc)glfwGetProcAddress), "Can't init opengl!");
+        }
+    }
+
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    while(!glfwWindowShouldClose(ctx.window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glfwPollEvents();
+        glfwSwapBuffers(ctx.window);
+
+        glfwGetWindowSize(ctx.window, &ctx.width, &ctx.height);
+        glViewport(0, 0, ctx.width, ctx.height);
+    }
+
+    // Cleanup
+    {
+        glfwDestroyWindow(ctx.window);
+        glfwTerminate();
+    }
 
     return 0;
 }
